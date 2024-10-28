4b9d45a259867d8490eafc7314acf6c228e1c7df
ellie-winer <elliewiner3@gmail.com>
2024-10-28 17:17:49 -0400

    trying to change the website to a card template for better organization, saved original template under index2.html to go back to

---
+++ projects.py
@@ -0,0 +1,116 @@
+# projects.py
+
+projects = [
+    {
+        "title": "Morse Code Converter",
+        "description": "Takes a text input including numbers, symbols, and spaces allowing for multiple words. The output is your text converted into morse code.",
+        "image": "project1.jpeg",
+        "github_link": "https://github.com/ellie-winer/MorseCode-Portfolio1",
+        "website_link": None
+    },
+    {
+        "title": "Portfolio Website",
+        "description": "This website displays my personal projects. Each project is shown with a description, image, and a link to the code on my GitHub.",
+        "image": "project2.jpeg",
+        "github_link": "https://github.com/ellie-winer/PortfolioWebsite-Portfolio2",
+        "website_link": None
+    },
+    {
+        "title": "Tic Tac Toe",
+        "description": "You can play against another person or the computer. Players alternate choosing which position on the grid to place their X or O until all places are filled and final results are announced.",
+        "image": "project3.jpeg",
+        "github_link": "https://github.com/ellie-winer/TicTacToe-Portfolio3",
+        "website_link": None
+    },
+    {
+        "title": "Watermark Application",
+        "description": "Adds the shown watermark image and takes an input as watermark text to any image you want. All you need to do is add your image to the application and input the filename.",
+        "image": "project4.jpeg",
+        "github_link": "https://github.com/ellie-winer/Watermark-Portfolio4",
+        "website_link": None
+    },
+    {
+        "title": "Typing Speed Test",
+        "description": "After pressing the start button, sentences will continually form until 60 seconds have passed. The user can type the sentences as quickly as possible, and after the timer is finished, the amount of correct words typed is displayed.",
+        "image": "project5.jpeg",
+        "github_link": "https://github.com/ellie-winer/TypingTest-Portfolio5",
+        "website_link": None
+    },
+    {
+        "title": "Breakout Game",
+        "description": "Fully functional and personally designed Breakout Game where the goal is to bounce the ball off the controller until all the bricks have been hit. Score and high score are recorded, and the controller moves left and right using keyboard arrow keys. Option to play again upon losing or winning!",
+        "image": "project6.jpeg",
+        "github_link": "https://github.com/ellie-winer/BreakoutGame-Portfolio6",
+        "website_link": None
+    },
+    {
+        "title": "Cafe and Wifi Website",
+        "description": "A website where you can see cafes and their information. Users can register an account and log in to add new cafes to the site or edit existing ones.",
+        "image": "project7.jpeg",
+        "github_link": "https://github.com/ellie-winer/CafeandWifiWebsite-Portfolio7",
+        "website_link": "https://elliewinercafeandwifi.onrender.com/"
+    },
+    {
+        "title": "Todo List Website",
+        "description": "A website where you can add items to your todo list with the option to add a due date and time. Check off items and watch them move from upcoming to completed. Register an account and log in to continually update your list and check your progress.",
+        "image": "project8.jpeg",
+        "github_link": "https://github.com/ellie-winer/TodoList-Portfolio8",
+        "website_link": "https://elliewinertodolist.onrender.com"
+    },
+    {
+        "title": "Disappearing Text Application",
+        "description": "A tool commonly used for writer's block, type whatever you want into the text box. However, you must keep typing because after 10 seconds of no keys being pressed, all that was written is deleted.",
+        "image": "project9.jpeg",
+        "github_link": "https://github.com/ellie-winer/DisappearingText-Portfolio9",
+        "website_link": None
+    },
+    {
+        "title": "PDF to Audiobook",
+        "description": "This script converts any uploaded PDF into an audio MP3 file. When you open and play the audio file, you will hear your text in your PDF read aloud like an audiobook.",
+        "image": "project10.jpeg",
+        "github_link": "https://github.com/ellie-winer/TexttoSpeech-Portfolio10",
+        "website_link": None
+    },
+    {
+        "title": "Image Color Palette Generator",
+        "description": "A website where you can upload an image and press analyze to see the top 10 most common colors in your image. Your image is displayed next to a data table with the results from the color analysis.",
+        "image": "project11.jpeg",
+        "github_link": "https://github.com/ellie-winer/ImageColorPaletteGenerator-Portfolio11",
+        "website_link": "https://elliewinerimagetocolor.onrender.com"
+    },
+    {
+        "title": "Custom Web Scraper",
+        "description": "Two custom web scraper applications that configure data from NBA top 5 teams and players and compare it against statistical parameters in a CSV table. One accounts for the 2023-24 fixed season data, and one changes daily with preseason data.",
+        "image": "project12.jpeg",
+        "github_link": "https://github.com/ellie-winer/CustomWebScraper-Portfolio12",
+        "website_link": None
+    },
+    {
+        "title": "Space Invaders Game",
+        "description": "A customized version of the classic game Space Invaders. The player must shoot all aliens to win, and the player has 3 lives. Players lose a life if hit by an alien bullet, and the game is over if all lives are lost or the aliens reach the player.",
+        "image": "project14.jpeg",
+        "github_link": "https://github.com/ellie-winer/SpaceInvaders-Portfolio14",
+        "website_link": None
+    },
+    {
+        "title": "Custom API Football Data Website",
+        "description": "A custom API website where you can see the Champions League matches and data. You can search for a team name or matchday to see all games related to your search. Data used is from football-data.org.",
+        "image": "project15.jpeg",
+        "github_link": "https://github.com/ellie-winer/CustomAPIWebsite-Portfolio15",
+        "website_link": "https://elliewinerapifbdata.onrender.com/"
+    },
+    {
+        "title": "Data Science - Analyze & Visualize Space Race",
+        "description": "Demonstrated data science skills such as data exploration, data cleaning, and data visualization in a Google Colab notebook using the provided Space Race dataset. The dataset was manipulated, analyzed, and turned into many chart forms to provide insight.",
+        "image": "project18.jpeg",
+        "github_link": "https://github.com/ellie-winer/AnalyzeandVisualizeSpaceRace-Portfolio18",
+        "website_link": None
+    },
+    {
+        "title": "Data Science - Analyze & Visualize US Deaths involving Police",
+        "description": "Demonstrated data science skills such as data exploration, data cleaning, and data visualization in a Google Colab notebook using the provided Washington Post dataset. The dataset was manipulated, analyzed, and turned into many chart forms to provide analysis and comparisons of many factors.",
+        "image": "project19.jpeg",
+        "github_link": "https://github.com/ellie-winer/AnalyzeUSDeaths-Portfolio19",
+        "website_link": None
+    }
+]
